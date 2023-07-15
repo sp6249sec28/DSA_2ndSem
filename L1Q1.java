24cca2cf7e386fb1f8d44b8b1fa62cff8e31c8e8
sp6249sec28 <129983317+sp6249sec28@users.noreply.github.com>
2023-07-15 10:48:28 +0530

    Update L1Q1.java

--- L1Q1.java
+++ L1Q1.java
@@ -6,11 +6,11 @@ public class L1Q1 {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a number: ");
 		int n = sc.nextInt();
-		double t1 = Math.log(n);
-		double t2 = Math.log(2);
-		int ans = (int)(t1/t2);
-		System.out.print(n + " must be divided by two " + ans + " times to get a value less than 2");
-		
+		int count = 0;
+		while(n!=0){
+			n=n/2;
+			count++;
 	}
+		System.out.println("The number of times 2 can be divided is " + count);
 
 }
